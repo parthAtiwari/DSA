07e5928b3f7af9fd203a54230d526247e233de2b
parthAtiwari <95771107+parthAtiwari@users.noreply.github.com>
2023-02-20 18:19:02 +0530

    Update IncrementBy1.py

--- Arrays/IncrementBy1.py
+++ Arrays/IncrementBy1.py
@@ -1,7 +1,7 @@
 # Write a program which takes as input an array of digits encoding a nonnegative decimal integer
-# D and updates the array to represent the integer D + 1. For example, if the input is (7,2,9) then
-# you should update the array to (1,3,0). Your algorithm should work even if it is implemented in a
-# langua ge that has finite-precision arithmetic.
+# D and updates the array to represent the integer D + 1. For example, if the input is [1,2,9] then
+# you should update the array to [1,3,0]. Your algorithm should work even if it is implemented in a
+# language that has finite-precision arithmetic.
 def plus_one(arr):
     arr[-1]+=1
     for i in reversed(range(1,len(arr))):
