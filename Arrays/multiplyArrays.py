1c0bd8e990acb9a3459a222c5fe7007ca48cbe27
parthAtiwari <95771107+parthAtiwari@users.noreply.github.com>
2023-02-20 18:15:36 +0530

    Update multiplyArrays.py

--- Arrays/multiplyArrays.py
+++ Arrays/multiplyArrays.py
@@ -1,5 +1,5 @@
 # Write a program that takes two arrays representing integers, and retums an integer representing their product. For example, since 193707721.x -761838257287 = -147573952589676412927
-# if the inputs are [1,9,3,7,0,7,7,2, 1] and [-7,6,L,8,3,8,2,5,7,2,8,7] your function should return
+# if the inputs are [1,9,3,7,0,7,7,2, 1] and [-7,6,1,8,3,8,2,5,7,2,8,7] your function should return
 # [-1, 4,7, 5,7,3, 9, 5, 2, 5,8,9, 6,7, 6, 4, 1., 2,9,2,7].
 def multiply(arr1,arr2):
     sign=-1 if ((arr1[0]<0)^(arr2[0]<0)) else 1
