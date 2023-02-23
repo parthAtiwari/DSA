85614e4adbf8ed400c2dd684bd78ffc0c5906e55
parthAtiwari <parthptiwari@gmail.com>
2023-02-24 00:49:54 +0530

    Day 4

--- Arrays/generate_primes.py
+++ Arrays/generate_primes.py
@@ -5,21 +5,30 @@
 # This algorithm does not exploit the fact that we do not need to compute all primes from 1 to n.
 # A better approach is to compute the prime and when a number is identified as a prime, to sieve it
 # i.e. remove all its multiples up to n from future consideration. We use a boolean array to encode the primes.
+import math
+
 
 def gen_primes(n):
     # since 0 and 1 are not primes
     primes = []
-    is_prime = [False, False]+[True for i in range(n-1)]
-    for p in range(2, n+1):
+    is_prime = [False, False] + [True for i in range(n - 1)]
+    for p in range(2, n + 1):
         if is_prime[p]:
             primes.append(p)
-            for m in range(p,n+1,p):
-                is_prime[m]=False
+            for m in range(p, n + 1, p):
+                is_prime[m] = False
     return primes
+
+
 # The above approach takes (n/2)+(n/3)+(n/5)+(n/7)+..... leading to O(n log log n)
-
-
-
-
-
-
+# Optimization
+def gen_primes(n):
+    # since 0 and 1 are not primes
+    primes = []
+    is_prime = [False, False] + [True for i in range(n - 1)]
+    for p in range(2, int(math.sqrt(n)) + 1):
+        if is_prime[p]:
+            primes.append(p)
+            for m in range(p * p, n + 1, p):
+                is_prime[m] = False
+    return primes
---
+++ Arrays/valid_sudoku.py
@@ -0,0 +1,20 @@
+# Valid Sudoku Leetcode 36
+def valid_sudoku(board):
+    def is_invalid(li):
+        return len(a := [i for i in li if i != '.']) != len(set(a))
+    # checking rows and columns
+    n=9
+    if any(is_invalid([board[r][c] for c in range(n)]) or is_invalid([board[c][r] for c in range(n)]) for r in range(n)):
+        return False
+
+    for i in range(3):
+        for j in range(3):
+            region=[]
+            for ix in range(i*3,(i+1)*3):
+                for jx in range(j*3,(j+1)*3):
+                    if board[ix][jx]!='.':
+                        region.append((board[ix][jx]))
+            if is_invalid(region):
+                return False
+    return True
+
